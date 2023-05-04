ffa2c91e745421865a91d566bbd0aa6078302ec1
Semos Akademija <akademija@semosedu.local>
2023-05-04 18:58:20 +0200

    chas2

---
+++ chas2/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <h1> ova e naslov preku html</h1>
+   <h1><?php echo "Ova e naslov prehu php" ; ?></h1>
+   <?php
+
+$text="ova e naslov preku php";
+$x=5;
+$t=101.5;
+$y=30;
+echo $text;
+
+
+$zbir= $x + $y
+
+
+
+
+
+
+
+//single line
+/*
+multi
+line
+comments
+*/
+
+//variables
+
+
+ ?>
+  <p> <?php echo $zbir?></p>
+</body>
+
+
+</html>
+
+<?php
+
+$text="ova e naslov preku php";
+$x=5;
+$t=101.5;
+echo $text;
+
+
+$zbir= $x + $y;
+
+
+echo " <h1><?phpOva e naslov prehu php ; ?></h1>";
+
+
+
+
+//single line
+/*
+multi
+line
+comments
+*/
+
+//variables
+
+
+ ?>
