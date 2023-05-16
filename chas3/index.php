272e7887752d3a8dd3f13b885fd886345c7a3422
Semos Akademija <akademija@semosedu.local>
2023-05-16 19:11:08 +0200

    chas3

---
+++ chas3/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  
+   <?php
+//operacii pomegju vrednosti i promenlivi
+// echo 4+4;
+
+// echo '<br>';
+
+// $x=6;
+// $y=5;
+
+// if ($x < $y){
+//     echo "x e pomalo od y";
+// } else {
+//     echo 'x e pogolemo od y';
+// }
+ 
+// echo '<br>';
+
+// $c=5;
+
+// echo ++$c;
+// echo '<br>';
+
+// echo  $c++ ;
+// echo '<br>';
+
+// $c=5;
+// echo --$c;
+// echo '<br>';
+// echo $c;
+// echo '<br>';
+
+// echo  $c-- ;
+// echo '<br>';
+// echo $c;
+// echo '<br>';
+// $var1= "sakam";
+// $var2= "cips";
+// echo $var1.$var2;
+// echo '<br>';
+// echo $var1.=$var2;
+
+
+// $a="dva";
+// $b="tri";
+// //  echo $a + $b;
+// //  echo$a - $b;
+
+// //  echo $a * $b;
+// //  echo $a / $b;
+
+// //  echo $a % $b;
+// echo '<br>';
+// $y="cetiri";
+// $broj_na_bukvi = strlen($y);
+// echo $broj_na_bukvi;
+
+// $zbor ="emilija";
+// $recenica="moze da se zeme emilija";
+// $pozicija=strpos($recenica,$zbor);
+// if ($position !== false) {
+//     echo  $position;
+// } else {
+//     echo "emilija ja nemit";
+// }
+
+$ime='pepita';
+$reversedString = strrev($ime);
+echo $reversedString;
+
+
+ ?>
